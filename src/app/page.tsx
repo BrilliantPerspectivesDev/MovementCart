d4ced0a1460cafde2380a492c8fa4e91ac2b2e07
AlarmclockUSA <matt@brilliantperspectives.com>
2025-03-27 20:09:56 -0400

    chore: Remove redundant src directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-  <Image
-    src="/Blacklogo.png" 
-    alt="Brilliant Logo"
-    width={120}
-    height={30}
-    className={`transition-opacity duration-300 ${scrolled ? 'opacity-100' : 'opacity-90'}`}
-    priority
-  /> 
